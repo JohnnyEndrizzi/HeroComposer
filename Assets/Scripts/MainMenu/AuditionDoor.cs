9362253a8daf68dc4496bf2f4b3cfee7ca1663d2
Yifu Wu <wu235@mcmaster.ca>
2018-11-19 15:15:41 -0500

    Fk

---
+++ Assets/Scripts/MainMenu/AuditionDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuditionDoor : MonoBehaviour {
+    private Sprite[] sprites;
+    public Animator auditionclose;
+   // public Canvas selectcanvas;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnMouseOver()
+    {
+       // if (selectcanvas.enabled == false)
+        //{
+            
+            auditionclose.Play("AuditionOpen");
+       // }
+    }
+
+    void OnMouseExit()
+    {
+       
+        auditionclose.Play("AuditionClose");
+    }
+}
