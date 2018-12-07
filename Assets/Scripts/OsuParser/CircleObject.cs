61823798d92a8452339a275e093fc301e33fd757
DamianAngelone <d-angelone@hotmail.com>
2018-12-07 03:07:44 -0500

    Removed unused files

--- Assets/Scripts/OsuParser/CircleObject.cs
+++ Assets/Scripts/OsuParser/CircleObject.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace OsuParser
 {
+    /* Class for circle objetcs (inherited from generic hit object) */
     class CircleObject : HitObject
     {
         public CircleObject(int x, int y, float startTime)
