514013fc48ed5faa8ac93f86eda418de0368a28e
endrizje <johnny.endrizzi@gmail.com>
2018-12-08 03:15:09 -0500

    Actually Finished stuff
    added sound effects
    moved files
    commented atkMove (i missed it before)

--- Assets/Scripts/AtkAnim/AtkMove.cs
+++ Assets/Scripts/AtkAnim/AtkMove.cs
@@ -3,9 +3,9 @@
 public class AtkMove : MonoBehaviour {
     //This script controls Animation movement 
 
+    //Initial and final values
     public Vector3 StartPos;
     public Vector3 EndPos;
-
     private Vector3 StartSize;
     public Vector3 EndSize;
 
@@ -15,6 +15,7 @@ public class AtkMove : MonoBehaviour {
     public float atkLerpTime;        
     float currentLerpTime = 0;
 
+    //Optional child mask
     private Transform mask;
     private Vector3 StartPosMask;
     private Vector3 EndPosMask;    
@@ -23,14 +24,14 @@ public class AtkMove : MonoBehaviour {
     {
         StartSize = transform.localScale;
 
-        //Custom commands for objects with masks
+        /*The second level of special attacks which control animation masks */        
         if (this.transform.childCount == 1) { 
             mask = this.transform.GetChild(0);
             StartPosMask = mask.localPosition;
             if      (custCmd == 51) {EndPosMask = new Vector3(StartPosMask.x + 1, StartPosMask.y, StartPosMask.z);}
             else if (custCmd == 52) {EndPosMask = new Vector3(StartPosMask.x, StartPosMask.y * -1, StartPosMask.z);}
             else if (custCmd == 53) {EndPosMask = StartPosMask; StartPosMask.y = StartPosMask.y * -1;}
-            else if (custCmd == 12) {EndPosMask = StartPosMask; counter = Random.Range(-5f, 5f);}
+            else if (custCmd == 12) {EndPosMask = StartPosMask; counter = Random.Range(-5f, 5f);} //it's not a mask, it's a child! 
         }
     }
 
@@ -43,20 +44,23 @@ public class AtkMove : MonoBehaviour {
                     
         float perc = currentLerpTime / atkLerpTime;
 
+        //Move attack 
         transform.position = Vector2.Lerp(StartPos, EndPos, perc);
         transform.localScale = Vector3.Lerp(transform.localScale, EndSize, perc);
 
+        //Face the target
         Vector3 targetDir = EndPos - StartPos;
         float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg-180;
         transform.localRotation = Quaternion.Euler(Vector3.forward * angle);
 
         //Custom commands during movement
-        if (custCmd == 4 & perc >= 1) {
+        /* Third level of special attacks which control unique timing patterns and spinning */
+        if (custCmd == 4 & perc >= 1) { //replay in reverse
             custCmd = -1; perc = 0;
             currentLerpTime = 0;
             EndSize = EndSize * 0.25f;
         }
-        if (custCmd == 3 & perc >= 1 & counter<2) {
+        if (custCmd == 3 & perc >= 1 & counter<2) { //triple move
             perc = 0; counter++;
             currentLerpTime = 0;
             transform.position = StartPos;
@@ -64,14 +68,14 @@ public class AtkMove : MonoBehaviour {
             EndPos.x = EndPos.x + Random.Range(-2f, 2f);
             EndPos.y = EndPos.y + Random.Range(-2f, 2f);
         }
-        if (custCmd == 12) {//TODO fix
+        if (custCmd == 12) {//spin child object
             mask.localRotation = Quaternion.Euler(Vector3.right*counter);
         }
 
         if (perc >= 1) { //destroy When complete
             Destroy(this.gameObject);
         }
-        if (mask != null) {
+        if (mask != null) { //Move mask
             mask.localPosition = Vector2.Lerp(StartPosMask, EndPosMask, perc);
         }
     }
