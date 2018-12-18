458289757b1bc567d0978abbbe028a26020f95e7
DamianAngelone <d-angelone@hotmail.com>
2018-12-18 03:36:21 -0500

    Got async scene loading to work

--- Assets/Scripts/StartScreen/ScreenLoader.cs
+++ Assets/Scripts/StartScreen/ScreenLoader.cs
@@ -7,41 +7,16 @@ public class ScreenLoader : MonoBehaviour
 {
     [SerializeField]
     private Button startBtn;
+
     [SerializeField]
     private Text loadingText;
+
     [SerializeField]
     private float delay = 1f; //audio fade out time
 
-    private bool clicked = false; // track user input
     private bool activated = false; //is button enabled (enables once curtains open)
 
     AsyncOperation async;
-
-    public void Start()
-    {
-        //StartCoroutine(Next());  
-    }
-
-    public void Click()
-    {
-        if (activated)
-        {
-            try { GameObject.Find("GObtn").transform.SetAsFirstSibling(); } catch { }
-            StartCoroutine(Next2());
-
-
-            clicked = true;
-            Debug.Log("C " + clicked);
-        }
-    }
-
-    IEnumerator Next2()
-    {
-        yield return StartCoroutine(AudioFadeOut()); //fade out audio before transition
-
-        SceneManager.LoadScene("Menu");
-        MenuSceneSwitch("Menu"); //switch scenes
-    }
 
     public void activation()
     {
@@ -52,59 +27,25 @@ public class ScreenLoader : MonoBehaviour
     IEnumerator Delay()
     {
         yield return new WaitForSeconds(1.0f);
-        yield return StartCoroutine(TextFader(1f, loadingText, 1, 0)); //disables "Loading..."
+        yield return StartCoroutine(TextFader(1f, loadingText, 1, 0));
     }
 
-    //Broken/disabled Async Code
-    IEnumerator Next()
-    {
-        yield return StartCoroutine(LoadNewScene()); //loads next scene in background
-        yield return StartCoroutine(TextFader(0.5f, loadingText, 1, 0)); //disables "Loading..."
-        startBtn.GetComponent<Button>().enabled = true;
-
-        yield return StartCoroutine(Clicked());      //waits until the button has been clicked once
-        yield return StartCoroutine(AudioFadeOut()); //fade out audio before transition
-
-        async.allowSceneActivation = true; //load async scene?
-        MenuSceneSwitch("Menu"); //switch scenes?
-
-        yield return null;
-    }    
-
-    IEnumerator Clicked()
-    {
-        while (!clicked)
-            yield return null;
-    }
-    
-    // The coroutine runs on its own at the same time as Update()
     IEnumerator LoadNewScene()
     {
-        StartCoroutine(TextFader(0.5f, loadingText, 0, 1)); //enables "Loading..."
-        startBtn.GetComponent<Button>().enabled = false;
-                
-        async = SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Additive);
-        async.allowSceneActivation = false; //stops async progress at 0.9 - doesnt run scripts
+        async = SceneManager.LoadSceneAsync("Menu");
+        async.allowSceneActivation = false;
 
-        while (async.progress < 0.9f)  
+        while (async.progress < 0.9f)
         {
             yield return null;
         }
+
+        yield return null;
     }
-    //Broken/disabled Async Code mostly ends here
-    
-    IEnumerator TextFader(float t, Text i, float fade1, float fade2) //Controls the text fading in or out for text 
+
+    private void MenuSceneSwitch(string sceneNew)
     {
-        //0 to invisible, 1 to visible - fader2 is opposite of fader
-        //fade1 = start alpha, fade2 = end alpha
-
-        i.color = new Color(i.color.r, i.color.g, i.color.b, fade1);
-
-        while (i.color.a < fade1 + 0.1 && i.color.a > fade2 - 0.1 || i.color.a < fade2 + 0.1 && i.color.a > fade1 - 0.1)
-        {
-            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t) * fade1 + (Time.deltaTime / t) * fade2);
-            yield return null;
-        }
+        GameObject.Find("sceneSwitcher").GetComponent<SceneSwitcher>().sceneSwitchCurtains(sceneNew);
     }
 
     IEnumerator AudioFadeOut()
@@ -120,8 +61,36 @@ public class ScreenLoader : MonoBehaviour
         }
     }
 
-    private void MenuSceneSwitch(string sceneNew) //SceneSwitch with curtains
+    IEnumerator TextFader(float t, Text i, float fade1, float fade2)
     {
-        GameObject.Find("sceneSwitcher").GetComponent<SceneSwitcher>().sceneSwitchCurtains(sceneNew);
+        //0 to invisible, 1 to visible - fader2 is opposite of fader
+        //fade1 = start alpha, fade2 = end alpha
+
+        i.color = new Color(i.color.r, i.color.g, i.color.b, fade1);
+
+        while (i.color.a < fade1 + 0.1 && i.color.a > fade2 - 0.1 || i.color.a < fade2 + 0.1 && i.color.a > fade1 - 0.1)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t) * fade1 + (Time.deltaTime / t) * fade2);
+            yield return null;
+        }
     }
+
+    IEnumerator Next()
+    {
+        startBtn.gameObject.SetActive(false);
+
+        yield return StartCoroutine(LoadNewScene());
+        yield return StartCoroutine(AudioFadeOut());
+        yield return StartCoroutine(TextFader(0.5f, loadingText, 1, 0));
+
+        async.allowSceneActivation = true;
+        MenuSceneSwitch("Menu");
+
+        yield return null;
+    }
+
+    public void Test()
+    {
+        StartCoroutine(Next());  
+    } 
 }
