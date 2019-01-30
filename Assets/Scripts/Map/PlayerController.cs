404bcc691afee25e15d1ee2fcea3e4aee8b26639
Nicolai Kozel <kozeln@mcmaster.ca>
2019-01-30 16:29:21 -0500

    Initial changes for Map elements.
    Not actually Parser-Fixes. This is the Map branch.

---
+++ Assets/Scripts/Map/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public LayerMask movementMask;
+    public Interactable focus; 
+
+    Camera cam;
+    PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            removeFocus();
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                motor.MoveToPoint(hit.point);
+                //Stop focusing any objects 
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if(interactable != null)
+                {
+                    setFocus(interactable);
+                }
+            }
+        }
+    }
+
+    void setFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if(focus != null)
+            {
+                focus.OnDefocused();
+            }
+            focus = newFocus;
+            motor.FollowTarget(focus);
+        }
+        newFocus.OnFocused(transform);
+    }
+
+    void removeFocus()
+    {
+        if(focus != null)
+        {
+            focus.OnDefocused();
+        }
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+}
---
+++ Assets/Scripts/Map/PlayerMotor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour {
+
+    NavMeshAgent agent;
+    Transform target;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+    private void Update()
+    {
+        if(target != null)
+        {
+            Vector3 targetPosition = target.position;
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
+
+    public void MoveToPoint (Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        agent.stoppingDistance = newTarget.radius * .8f;
+        agent.updateRotation = false;
+        target = newTarget.interactionTransform;
+    }
+    
+    public void StopFollowingTarget()
+    {
+        agent.stoppingDistance = 0f;
+        agent.updateRotation = true;
+        target = null;
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime*5f);
+    }
+}
