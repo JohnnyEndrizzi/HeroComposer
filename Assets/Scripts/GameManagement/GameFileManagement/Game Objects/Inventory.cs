92fd0257d1f2646399e64e404a544c6d5c193b9d
Nicolai Kozel <nicolaikozel@live.ca>
2019-03-17 10:47:27 -0400

    Adding comments.

--- Assets/Scripts/GameManagement/GameFileManagement/Game Objects/Inventory.cs
+++ Assets/Scripts/GameManagement/GameFileManagement/Game Objects/Inventory.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 
 [System.Serializable]
 public class Inventory{
+    //Player's available money
     public int money;
+    //Player's inventory of items
     public List<Item> items;
 
+    //Add item to inventory
     public void AddItem(Item item)
     {
         //Create list if it doesn't exist yet
