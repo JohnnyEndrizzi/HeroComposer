8a3d4509ad928f7baa1c5e8e449e6b224869c732
pavlearezina <pavlearezina@outlook.com>
2019-03-18 21:23:49 -0400

    Yifu changes

--- Assets/Scripts/Map/Up_Down.cs
+++ Assets/Scripts/Map/Up_Down.cs
@@ -2,29 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Up_Down : MonoBehaviour {
-	private bool movement_lock;
-	private int direction;
-	private float init_position;
-	private float deltay;
-	public float delay_time;
-	// Use this for initialization
-	void Start () {
-		movement_lock = false;
+public class Up_Down : MonoBehaviour
+{
+    private bool movement_lock;
+    private int direction;
+    private float init_position;
+    private float deltay;
+    public float delay_time;
+    private float init_deltay;
+    // Use this for initialization
+    void Start()
+    {
+        movement_lock = false;
         init_position = this.transform.position.y;
-        deltay = 0.0f;
+        init_deltay = 0.0f;
+        deltay = init_deltay;
         /*Randomly pick a starting direction*/
-        direction=1;
+        direction = 1;
 
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.LookAt(Camera.main.transform.position, Vector3.up);
-		if (Input.GetKey("up"))
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform.position, Vector3.up);
+        if (Input.GetKey("up"))
         {
- 
+
         }
         else if (Input.GetKey("down"))
         {
@@ -43,45 +48,54 @@ public class Up_Down : MonoBehaviour {
         }
         //updown();
         StartCoroutine(updown(delay_time));
-	}
+    }
 
-	float movement(float x, int direction)
-	{
-		float y = 0;
-		if (direction == 1 && x < 5){
-			y = 0.1f;
-		}
-		else if (direction == 1 && x >= 5){
-			y = -0.1f;
-			this.direction = -1;
-			Debug.Log("change direction");
-		}
-		else if (direction == -1 && x >0){
+    float movement(int direction)
+    {
+        float y = 0;
+        if (direction == 1 && deltay < 2)
+        {
+            deltay = deltay + 0.1f;
+            y = 0.1f;
+        }
+        else if (direction == 1 && deltay >= 2)
+        {
+            deltay = deltay - 0.1f;
+            this.direction = -1;
+            Debug.Log("change direction");
+            y = -0.1f;
+        }
+        else if (direction == -1 && deltay > 0)
+        {
 
-			y = -0.1f;
-		}
-		else if (direction == -1 && x <=0){
-			y = 0.1f;
-			this.direction = 1;
-			Debug.Log("change direction");
-		}
+            deltay = deltay - 0.1f;
+            y = -0.1f;
+        }
+        else if (direction == -1 && deltay <= 0)
+        {
+            deltay = deltay + 0.1f;
+            this.direction = 1;
+            Debug.Log("change direction");
+            y = 0.1f;
+        }
 
-		return y;
-		//Mathf.SmoothStep(0, Mathf.Sin(Random.Range(-50, 50)*Time.deltaTime), 99)
+        return y;
+        //Mathf.SmoothStep(0, Mathf.Sin(Random.Range(-50, 50)*Time.deltaTime), 99)
 
-	}
+    }
 
-	//void updown()
-	 IEnumerator updown(float time)
-	{
-	yield return new WaitForSeconds(time);
-		//Debug.Log("current is  " + this.transform.position.y + " init is " + init_position + " deltay is " + deltay + " direction is " + direction);
-		
-		float newy = this.transform.position.y + movement(deltay,direction);
-		deltay = this.transform.position.y - init_position;
-	    this.transform.position = new Vector3(transform.position.x, newy, transform.position.z);
-	 
-	} 
+    //void updown()
+    IEnumerator updown(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Debug.Log("current is  " + this.transform.position.y + " deltay is " + deltay + " direction is " + direction);
+        //Debug.Log(deltay);
+
+        float newy = this.transform.position.y + movement(direction);
+        //deltay = this.transform.position.y - init_position;
+        this.transform.position = new Vector3(transform.position.x, newy, transform.position.z);
+
+    }
 
 
 }
